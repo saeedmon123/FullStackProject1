a193849b1f6ffe50fce8f776955f44bd8b352302
Mohammad said monawar <mhmsaeedprogram26@gmail.com>
2024-12-28 17:37:58 +0200

    last_update

--- react-frontend/tailwind.config.js
+++ react-frontend/tailwind.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Ensure this path matches your project structure
+  ],
   theme: {
     extend: {},
   },
