21729a5091e243a2d5e48a743b04b2e205f9efbd
Mohammad said monawar <mhmsaeedprogram26@gmail.com>
2024-12-28 18:31:53 +0200

    Update api.js

--- react-frontend/src/api.js
+++ react-frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://127.0.0.1:8000/api/elements";
+const API_URL = "https://fullstackproject1-1.onrender.com/api/elements";
 
 export const getElements = async (filters = {}) => {
     const { data } = await axios.get(API_URL, { params: filters });
