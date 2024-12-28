0f2a54f6400c78883b70c848d4c41cdcfcedcec4
Mohammad said monawar <mhmsaeedprogram26@gmail.com>
2024-12-28 09:28:37 +0200

    Update settings.py

--- For_Father/For_Father/settings.py
+++ For_Father/For_Father/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u675nyn3xjli3e6jr6z0779&*=grzl4%kqsagl^xpg3d2k$5^5'
+from decouple import config
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+SECRET_KEY = config('django-insecure-u675nyn3xjli3e6jr6z0779&*=grzl4%kqsagl^xpg3d2k$5^5')
+DEBUG = config('DEBUG', default=False, cast=bool)
+ALLOWED_HOSTS = config('*', default='', cast=lambda v: [s.strip() for s in v.split(',')])
 
-ALLOWED_HOSTS = ['*']
 
 
 # Application definition
